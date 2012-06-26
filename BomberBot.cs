944d7b6309ed4179685f0e4908f5ca9c4ed8dd9e
Julian Vargas <julianvargaslvarez@gmail.com>
2012-06-26 00:15:33 -0500

    initial client running but not fully tested

---
+++ BomberBot.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace cSharpbot
+{
+    class BomberBot
+    {
+        static void Main(string[] args)
+        {
+            new BomberBot();            
+        }
+
+        byte[] inFromServer = new byte[2048];
+        private Socket socketCliente = null;
+        private Bot bot = null;
+
+        private bool conectado = false;
+
+        public BomberBot()
+        {
+            try
+            {
+                conectar("cSharpBot", "984198716");
+                controlConexion();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void conectar(String user, String token)
+        {
+            socketCliente = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socketCliente.Connect("localhost", 5000);
+
+            socketCliente.Receive(inFromServer);
+            String bienvenida = Encoding.UTF8.GetString(inFromServer);
+            Console.WriteLine(bienvenida);
+
+            byte[] msg = System.Text.Encoding.UTF8.GetBytes(user + "," + token);
+
+            socketCliente.Send(msg, msg.Length, SocketFlags.None);
+            conectado = true;
+        }
+
+        private void controlConexion()
+        {
+            byte[] response = new byte[2048];
+            while (conectado)
+            {
+                Console.WriteLine("turno");
+
+                socketCliente.Receive(response);
+                String mensaje = Encoding.UTF8.GetString(response);
+                string[] serverMessage = Regex.Split(mensaje, "\r\n");
+                string[] message = Regex.Split(serverMessage[0], ";");
+                if (message[0] == "EMPEZO")
+                {
+                    bot = new Bot(message[2][0]);
+                    bot.updateMap(message[1]);
+                }
+                else if (message[0] == "TURNO")
+                {
+                    Console.WriteLine("turno: " + message[1]);
+                    bot.updateMap(message[2]);
+                    byte[] msg = System.Text.Encoding.UTF8.GetBytes(bot.move());
+                    socketCliente.Send(msg, msg.Length, SocketFlags.None);
+                }
+                else if (message[0] == "PERDIO")
+                {
+                    Console.WriteLine("perdi :(");
+                }
+
+            }
+        }
+    }
+}
---
+++ Bot.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace cSharpbot
+{
+    class Bot
+    {
+        private char letter = ' ';
+        private int x;
+        private int y;
+        private String[][] map;
+
+        public Bot(char letter)
+        {
+            this.letter = letter;
+
+        }
+
+        public void updateMap(string mapa)
+        {
+            string[] rows = Regex.Split(mapa, "\n");
+            map = new String[rows.Length][];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                map[i] = Regex.Split(rows[i], ",");
+            }
+            int pos = mapa.IndexOf(this.letter) / 2;
+            this.y = pos / rows.Length;
+            this.x = pos % rows.Length;
+        }
+
+        public String move()
+        {
+            Random random = new Random();
+            int mov = random.Next(0, 7);
+            Console.WriteLine("mov " + mov);
+            switch (mov)
+            {
+                case 0:
+                    return "N";
+                case 1:
+                    return "E";
+                case 2:
+                    return "S";
+                case 3:
+                    return "O";
+                case 4:
+                    return "BN";
+                case 5:
+                    return "BE";
+                case 6:
+                    return "BS";
+                case 7:
+                    return "BO";
+            }
+            return "P";
+        }
+
+    }
+}
